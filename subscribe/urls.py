1db1c4a6ed8331e62a902aa4dd75f138ec85eb94
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-06-22 14:06:02 +0000

    Added subscription plan page with some functionality

---
+++ subscribe/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.view_subscription_plan, name='view_subcription_plan'), 
+]
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
+from .models import Plan
 # Create your views here.
+def view_subscription_plan(request):
+    """ A view that renders the subcription contents page """
+    plans = Plan.objects.all()
+    context = {
+        'plans': plans,
+    }
+    return render(request, 'subscribe/subscribtion_plans.html', context)
+
