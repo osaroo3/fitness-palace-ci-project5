1a3416ba332199450bb3c139292e4cbc3e2362e7
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-07-18 06:51:28 +0000

    Deployment test

--- fitness_palace/settings.py
+++ fitness_palace/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 #import dj_database_url
 import sys
@@ -32,9 +33,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-osaroo3-fitnesspalaceci-fwddorzlr03.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-osaroo3-fitnesspalaceci-fwddorzlr03.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 # Application definition
 
@@ -131,13 +132,23 @@ WSGI_APPLICATION = 'fitness_palace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
