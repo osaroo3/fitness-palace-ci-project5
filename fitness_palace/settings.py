2e6c0b6efb22d3cfd304136dec81a4114ce32f03
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-07-18 07:10:42 +0000

    Deployment test

--- fitness_palace/settings.py
+++ fitness_palace/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-osaroo3-fitnesspalaceci-fwddorzlr03.ws.codeinstitute-ide.net', '.herokuapp.com']
 
