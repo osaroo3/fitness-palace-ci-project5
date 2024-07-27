eaf19db9bb0d42dac898ac9841707c923f1819bd
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-07-27 07:01:21 +0000

    Added login decorator to my plans page

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Plan
 # Create your views here.
 
-
+@login_required
 def view_subscription_plan(request):
     """ A view that renders the subcription contents page """
     plans = Plan.objects.all()
